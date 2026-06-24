fb2ad5702ad5fedf0c2a3f2b2fd76afc78376a40
David SPORN <sporniket.studio@gmail.com>
2026-06-24 05:56:12 +0200

    cli: script to print a frontmatter, to inject in a new markdown file ; does not include 'since' or 'milestone' tags

---
+++ print-frontmatter.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "---"
+echo "creation-date: $(date -Idate)"
+echo "copyright-years: $(date +%Y)"
+echo "copyright-authors: David SPORN"
+echo "SPDX-License-Identifier: AGPL-3.0-or-later"
+echo "---"
+
